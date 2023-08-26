8e3739147a56ee6ef3e923039ac75371bed4ace4
davidk <dkato81@gmail.com>
2023-08-26 11:24:17 +0300

    Reqister models with admin site

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
 
 class LessonInline(admin.StackedInline):
     model = Lesson
+    extra = 5
+
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 5
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
     extra = 5
 
 
@@ -26,5 +36,8 @@ class LessonAdmin(admin.ModelAdmin):
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Question, Choice)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+
+admin.site.register(QuestionInline, ChoiceInline)
